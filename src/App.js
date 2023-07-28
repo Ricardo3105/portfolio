e2ea13aee84c4d9f4fe21e04669d5a8625453171
Ricardo Arias <ricardoarias.2012@hotmail.com>
2023-07-28 01:22:34 -0500

    se sube a github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
 import Container from "./components/Container/Container";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
@@ -7,10 +7,10 @@ import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <>  
+    <>
       <NavBar />
       <Container>
-        <AboutMe/>
+        <AboutMe />
         <Projects />
         <Contact />
       </Container>
--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,58 +3,66 @@ import * as S from "./AboutMe.styles";
 import logoGithub from "../../svg/github.svg";
 import logoLinkeind from "../../svg/linkedin.svg";
 import logoYoutube from "../../svg/youtube.svg";
+import foto from "../../images/projects/foto.jpg";
 
 const AboutMe = () => {
-    const socialNetworks =[
-        {
-            name:"Github",
-            link:"https://github.com/ivandevia",
-            logo:logoGithub,
-        },
-        {
-            name:"Linkedind",
-            link:"www.linkedin.com/in/ivan-devia-acosta-38b9aa141",
-            logo:logoLinkeind,
-        },
-        {
-            name:"Youtube",
-            link:"https://www.youtube.com/channel/UC2Gv4AvSJOPFQ6pjGFk7U2g",
-            logo:logoYoutube,
-        },
-    ];
+  const socialNetworks = [
+    {
+      name: "Github",
+      link: "https://github.com/Ricardo3105",
+      logo: logoGithub,
+    },
+    {
+      name: "Linkedind",
+      link: "https://www.linkedin.com/in/ricardo-arias-591398166/",
+      logo: logoLinkeind,
+    },
+    // {
+    //   name: "Youtube",
+    //   link: "https://www.youtube.com/channel/UC2Gv4AvSJOPFQ6pjGFk7U2g",
+    //   logo: logoYoutube,
+    // },
+  ];
 
-    return (
-        <S.ContainerAboutMe id="about-me">
-            <S.ContainerFlex>
-                <S.ContainerText>
-                    <S.Title>
-                        <p>👋🏼 Hello, i'm</p>
-                        <p>Iván Devia Acosta</p>
-                        <p>Backend Developer</p>
-                    </S.Title>
-                    <S.DescriptionText>
-                    Ing. de Sistemas, Mg. en Gestión de Tecnología Educativa, 
-                    Maestrante en Dirección Estratégica en Ing de Software, 
-                    con 17 años de experiencia en el sector productivo y educativo.
-                    </S.DescriptionText>
-                    <S.ContainerIcons>
-                    {socialNetworks.map((network, index)=>(
-                        <a
-                            key={index}
-                            href={network.link}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img src={network.logo} alt={network.name}/>
-                        </a>
-                    ))}
-                </S.ContainerIcons>
-                </S.ContainerText>
-                <S.ContainerImage>
-                    <img src="/img/Logo.png" alt="Iván Devia - Backend Developer"/>
-                </S.ContainerImage>
-            </S.ContainerFlex>
-        </S.ContainerAboutMe>
-    );
+  return (
+    <S.ContainerAboutMe id="about-me" style={{ marginTop: 85 }}>
+      <S.ContainerFlex>
+        <S.ContainerText>
+          <S.Title>
+            <p>👋🏼 Hello, i'm</p>
+            <p>Ricardo Arias</p>
+            <p>Front-end Developer</p>
+          </S.Title>
+          <S.DescriptionText>
+            <p>
+              ¡Hola! Soy un ingeniero de sistemas apasionado por el mundo del
+              desarrollo y con más de 4 años de experiencia en el campo. Soy una
+              persona proactiva y entusiasta, siempre buscando aprender cosas
+              nuevas y enfrentar nuevos desafíos en mi carrera profesional.
+            </p>
+            <p>
+              ¡Gracias por visitar mi portfolio! Si deseas conocer más sobre mis
+              proyectos y experiencias, no dudes en contactarme.
+            </p>
+          </S.DescriptionText>
+          <S.ContainerIcons>
+            {socialNetworks.map((network, index) => (
+              <a
+                key={index}
+                href={network.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={network.logo} alt={network.name} />
+              </a>
+            ))}
+          </S.ContainerIcons>
+        </S.ContainerText>
+        <S.ContainerImage>
+          <img src={foto} alt="Ricardo Arias" />
+        </S.ContainerImage>
+      </S.ContainerFlex>
+    </S.ContainerAboutMe>
+  );
 };
 export default AboutMe;
